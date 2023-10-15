3a9257025a3bc9582b17ada3cfcb95c88c1a1598
伍小威 <2463359862@qq.com>
2023-10-15 20:50:13 +0800

    商品详细信息管理

--- service/service-product/src/main/java/com/mall/wxw/product/service/SkuInfoService.java
+++ service/service-product/src/main/java/com/mall/wxw/product/service/SkuInfoService.java
@@ -1,8 +1,12 @@
 package com.mall.wxw.product.service;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.mall.wxw.model.product.SkuInfo;
+import com.mall.wxw.vo.product.SkuInfoQueryVo;
+import com.mall.wxw.vo.product.SkuInfoVo;
 
 /**
  * <p>
@@ -14,4 +18,7 @@ import com.mall.wxw.model.product.SkuInfo;
  */
 public interface SkuInfoService extends IService<SkuInfo> {
 
+    IPage<SkuInfo> selectPage(Page<SkuInfo> pageParam, SkuInfoQueryVo skuInfoQueryVo);
+
+    void saveSkuInfo(SkuInfoVo skuInfoVo);
 }
--- service/service-product/src/main/java/com/mall/wxw/product/service/impl/SkuInfoServiceImpl.java
+++ service/service-product/src/main/java/com/mall/wxw/product/service/impl/SkuInfoServiceImpl.java
@@ -1,11 +1,29 @@
 package com.mall.wxw.product.service.impl;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.mall.wxw.model.product.SkuAttrValue;
+import com.mall.wxw.model.product.SkuImage;
 import com.mall.wxw.model.product.SkuInfo;
+import com.mall.wxw.model.product.SkuPoster;
 import com.mall.wxw.product.mapper.SkuInfoMapper;
+import com.mall.wxw.product.service.SkuAttrValueService;
+import com.mall.wxw.product.service.SkuImageService;
 import com.mall.wxw.product.service.SkuInfoService;
+import com.mall.wxw.product.service.SkuPosterService;
+import com.mall.wxw.vo.product.SkuInfoQueryVo;
+import com.mall.wxw.vo.product.SkuInfoVo;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -17,5 +35,77 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoMapper, SkuInfo> implements SkuInfoService {
+    @Resource
+    private SkuPosterService skuPosterService;
 
+    @Resource
+    private SkuImageService skuImagesService;
+
+    @Resource
+    private SkuAttrValueService skuAttrValueService;
+
+    //获取sku分页列表
+    @Override
+    public IPage<SkuInfo> selectPage(Page<SkuInfo> pageParam, SkuInfoQueryVo skuInfoQueryVo) {
+        //获取条件值
+        String keyword = skuInfoQueryVo.getKeyword();
+        String skuType = skuInfoQueryVo.getSkuType();
+        Long categoryId = skuInfoQueryVo.getCategoryId();
+        //封装条件
+        LambdaQueryWrapper<SkuInfo> wrapper = new LambdaQueryWrapper<>();
+        if(!StringUtils.isEmpty(keyword)) {
+            wrapper.like(SkuInfo::getSkuName,keyword);
+        }
+        if(!StringUtils.isEmpty(skuType)) {
+            wrapper.eq(SkuInfo::getSkuType,skuType);
+        }
+        if(!StringUtils.isEmpty(categoryId)) {
+            wrapper.eq(SkuInfo::getCategoryId,categoryId);
+        }
+        //调用方法查询
+        return baseMapper.selectPage(pageParam, wrapper);
+    }
+
+    //添加商品
+    @Transactional(rollbackFor = {Exception.class})
+    @Override
+    public void saveSkuInfo(SkuInfoVo skuInfoVo) {
+        //保存sku信息
+        SkuInfo skuInfo = new SkuInfo();
+        BeanUtils.copyProperties(skuInfoVo, skuInfo);
+        this.save(skuInfo);
+
+        //保存sku海报
+        List<SkuPoster> skuPosterList = skuInfoVo.getSkuPosterList();
+        if(!CollectionUtils.isEmpty(skuPosterList)) {
+            for(SkuPoster skuPoster : skuPosterList) {
+                skuPoster.setSkuId(skuInfo.getId());
+            }
+            skuPosterService.saveBatch(skuPosterList);
+        }
+
+        //保存sku图片
+        List<SkuImage> skuImagesList = skuInfoVo.getSkuImagesList();
+        if(!CollectionUtils.isEmpty(skuImagesList)) {
+            int sort = 1;
+            for(SkuImage skuImages : skuImagesList) {
+                skuImages.setSkuId(skuInfo.getId());
+                skuImages.setSort(sort);
+                sort++;
+            }
+            skuImagesService.saveBatch(skuImagesList);
+        }
+
+        //保存sku平台属性
+        List<SkuAttrValue> skuAttrValueList = skuInfoVo.getSkuAttrValueList();
+        if(!CollectionUtils.isEmpty(skuAttrValueList)) {
+            int sort = 1;
+            for(SkuAttrValue skuAttrValue : skuAttrValueList) {
+                skuAttrValue.setSkuId(skuInfo.getId());
+                skuAttrValue.setSort(sort);
+                sort++;
+            }
+            skuAttrValueService.saveBatch(skuAttrValueList);
+        }
+    }
 }
