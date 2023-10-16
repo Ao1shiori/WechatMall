ff1d34bbb515f6e8d01755cb02f2f349949a7918
伍小威 <2463359862@qq.com>
2023-10-16 12:53:47 +0800

    Nacos服务注册

--- service/service-sys/src/main/java/com/mall/wxw/ServiceSysApplication.java
+++ service/service-sys/src/main/java/com/mall/wxw/ServiceSysApplication.java
@@ -2,11 +2,13 @@ package com.mall.wxw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author: wxw24633
  * @Time: 2023/10/15  16:07
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceSysApplication {
     public static void main(String[] args) {
